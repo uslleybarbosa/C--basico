7f0bfdeaa77d48f506f51a4324d00e8615df6b1b
WeslleyBarbosa <uslleybarbosa@gmail.com>
2025-04-27 12:22:26 -0300

    nova pasta

---
+++ caracteres.c/caracteres.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    printf("Hello Word\n");
+    printf("Tenho 10%% \n");
+    printf("Tabela01 \t");
+    printf("tabela02\t\n");
+    system("PAUSE");
+    return 0;
+}
